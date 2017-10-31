81e69340c119c4083c5c9083e37e62c6f29f6d0c
Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>
2017-10-31 19:54:11 +0000

    Added feature - filter A record

--- dnsshield.rb
+++ dnsshield.rb
@@ -10,6 +10,7 @@ opts = Trollop::options do
       opt :port, "Port to listen to", :type => :int, :default => 53
       opt :interface, "Interface to which to listen to", :type => :string, :default => "0.0.0.0"
       opt :loglevel, "Log level - DEBUG, INFO, etc", :type => :string, :default => "INFO"
+      opt :filteraddress, "Which A record to filter for", :type => :string, :default => "0.0.0.0"
 end
 
 UPSTREAM = RubyDNS::Resolver.new([[:udp, opts[:upstream], 53], [:tcp, opts[:upstream], 53]])
@@ -22,11 +23,17 @@ RubyDNS::run_server(:listen => INTERFACES) do
     on(:start) do
       @logger.level = eval("Logger::#{opts[:loglevel]}")
     end
-    Name = Resolv::DNS::Name
-    IN = Resolv::DNS::Resource::IN  
 
     match(/#{opts[:zone]}$/) do |transaction, match_data| 
-        transaction.passthrough!(UPSTREAM)
+        r = UPSTREAM.query(transaction.name, transaction.resource_class)
+        # drop a specific, invalid response
+        if (  r.answer.length>0 and 
+              r.answer[0][2].respond_to?(:address) and
+              r.answer[0][2].address.to_s=="0.0.0.0") then 
+           transaction.fail!(:NXDomain)
+        else
+          transaction.response.merge!(r)
+        end
     end
 
     otherwise do |transaction| 
